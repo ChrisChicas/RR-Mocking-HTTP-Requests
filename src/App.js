952408c7737ecb5062522068c41fa5c4337cf796
Chris Chicas <christianchicas7@gmail.com>
2022-03-30 03:52:02 -0400

    test written for mock http requests using jest-fetch-mock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import './App.css'
+import {useState, useEffect} from "react"
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState("")
+  const [gitHubURL, setGitHubURL] = useState("")
+  const [gitHubPic, setGitHubPic] = useState("")
+
+  useEffect(async () => {
+    const response = await fetch("https://api.github.com/users/ChrisChicas")
+    const resData = await response.json()
+    setGitHubName(resData.name)
+    setGitHubURL(resData.html_url)
+    setGitHubPic(resData.avatar_url)
+  }, [])
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a href={gitHubURL}>
+        <button>Link to GitHub Profile</button>
+      </a>
+      <img src={gitHubPic} alt="users pfp"/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,33 @@
+import App from "./App"
+import {render, screen, waitFor} from "@testing-library/react"
 
+beforeEach(() => {
+    fetch.resetMocks()
+})
 
+describe("testing mock fetch request", () => {
+    test("recieves GitHub name from GitHub REST API using jest fetch mock", async () => {
+        fetch.mockResponseOnce(JSON.stringify({name: "coder"}))
+        render(<App/>)
+        const gitHubName = await waitFor(() => screen.getByRole("heading", {level: 2}))
+        expect(gitHubName).toHaveTextContent("coder")
+    })
+})
 
+describe("pt. 2", () => {
+    test("recieves GitHub url from GitHubREST API using jest fetch mock", async () => {
+        fetch.mockResponseOnce(JSON.stringify({html_url: "https://github.com/test"}))
+        render(<App/>)
+        const gitHubURL = await waitFor(() => screen.findByRole("link"))
+        expect(gitHubURL).toHaveAttribute("href", "https://github.com/test")
+    })
+})
+
+describe("pt. 3", () => {
+    test("recieves GitHub picture url from GitHubREST API using jest fetch mock", async () => {
+        fetch.mockResponseOnce(JSON.stringify({avatar_url: "https://github.com/picturetest"}))
+        render(<App/>)
+        const gitHubURL = await waitFor(() => screen.findByRole("img"))
+        expect(gitHubURL).toHaveAttribute("src", "https://github.com/picturetest")
+    })
+})
